f6eb282a87c5cd9701b69133ee66658a6e35dd85
YuyaMurao <45620590+YuyaMurao@users.noreply.github.com>
2019-05-29 20:37:43 +0900

    Add files via upload

--- joke/The_honest_woodcutter.java
+++ joke/The_honest_woodcutter.java
@@ -1,3 +1,11 @@
+/*
+ただ出力をぬるぽにしたかっただけ
+出力:
+きこりは手を滑らせなかったため斧を湖に落としませんでした。
+女神が現れ、きこりに尋ねた。「あなたが落としたのは
+Exception in thread "main" java.lang.NullPointerException
+    at The_honest_woodcutter.main(The_honest_woodcutter.java:6)
+ */
 public class The_honest_woodcutter {
     public static void main(String[] args) {
         String str = null;
