c12950279912435903169cd79b0edd5e8b2008e2
YuyaMurao <45620590+YuyaMurao@users.noreply.github.com>
2019-05-29 20:35:29 +0900

    Add files via upload

---
+++ joke/The_honest_woodcutter.java
@@ -0,0 +1,9 @@
+public class The_honest_woodcutter {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println("きこりは手を滑らせなかったため斧を湖に落としませんでした。");
+        System.out.print("女神が現れ、きこりに尋ねた。「あなたが落としたのは");
+        System.out.print(str.toString());
+        System.out.print("ですね？");
+    }
+}
